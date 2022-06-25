ee89a6ea899dbdc5c54a7152a5a609415c9a209f
salamone-gius <salamone.gius@gmail.com>
2022-06-25 15:34:22 +0200

    basic style added

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="style/style.css">
         <title>Google Faq</title>
     </head>
@@ -16,8 +22,12 @@
         <main>
             <div class="container">
                 <?php foreach ($database as $post) { ?>
-                    <?php echo $post["question"]; ?>
-                    <?php echo $post["answer"]; ?>
+                    <div class="question-box">
+                        <?php echo $post["question"]; ?>
+                    </div>
+                    <div class="answer-box">
+                        <?php echo $post["answer"]; ?>
+                    </div>
                 <?php } ?>
             </div>
         </main>      
